69dc08971e7e2515a13dae1837ce0c83b09a3c55
yarikthe5 <153199325+yarikthe5@users.noreply.github.com>
2026-06-07 15:10:40 +0300

    Delete yarchat directory

--- yarchat/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://tpeidrgkqcozxhsuyezs.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwZWlkcmdrcWNvenhoc3V5ZXpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4MjE4OTYsImV4cCI6MjA5NjM5Nzg5Nn0.QPOmFN8jgbMfTL2SxfNawsXyHhIJHA-I6dcV5WPPZbk";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
